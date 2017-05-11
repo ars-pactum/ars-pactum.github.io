70bd508f32442939952659e628d5c7d8a687d53d
Sarah Tran <Sarah@seantrans-iMac.local>
2017-05-11 14:06:32 -0500

    a

--- java.js
+++ java.js
@@ -1,30 +1,41 @@
 function init(){
- pullINFO(STUDENT);
-}
-
-function pullINFO(KEYWORD) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-            setSTUDENT(this);
-    }
-  };
-  xhttp.open("GET", "catalog.xml", true);
-  xhttp.send();
-}
-
-function setSTUDENT(xml) {
-  var xmlSKILL;
-  var xmlDOC = xml.responseXML;
-  var x = xmlDOC.getElementsByTagName("student");
-  for (i = 0; i <x.length; i++) {
-      var n = x[i].getElementsByTagName("fname")[0].childNodes[0].nodeValue;
-      var l = x[i].getElementsByTagName("lname")[0].childNodes[0].nodeValue;
-      xmlSKILL += n + ",";
-      xmlSKILL += l + "<br>";
-
+  function createSTUDENT(first,last,type){
+    this.first = first;
+    this.last = last;
+    this.type = last;
   }
-    document.getElementById("cast").innerHTML = xmlSKILL;
+
+  var pamela = new createSTUDENT('Pamela','Roux','Magician');
+
+  var cast = document.getElementById('cast');
+  cast.textContent = pamela.last;
 }
+
+// function pullINFO(KEYWORD) {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//             setSTUDENT(this);
+//     }
+//   };
+//   xhttp.open("GET", "catalog.xml", true);
+//   xhttp.send();
+// }
+//
+// function setSTUDENT(xml) {
+//   var xmlSKILL;
+//   var xmlDOC = xml.responseXML;
+//   var x = xmlDOC.getElementsByTagName("student");
+//   for (i = 0; i <x.length; i++) {
+//       var n = x[i].getElementsByTagName("fname")[0].childNodes[0].nodeValue;
+//       var l = x[i].getElementsByTagName("lname")[0].childNodes[0].nodeValue;
+//       xmlSKILL += n + ",";
+//       xmlSKILL += l + "<br>";
+//
+//   }
+//     document.getElementById("cast").innerHTML = xmlSKILL;
+// }
+
+
 
 window.onload = init;
