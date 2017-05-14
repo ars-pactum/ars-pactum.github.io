990c1b3ad3b1a0cb57545a066ccf791e37424958
Sarah Tran <Sarah@seantrans-iMac.local>
2017-05-13 23:31:51 -0500

    looks at these changes

--- js/java.js
+++ js/java.js
@@ -5,10 +5,15 @@ function init(){
   document.getElementById('Human').onclick = function(){generateCast("Human");};
   document.getElementById('Entity').onclick = function(){generateCast("Entity");};
 
-createINFORMATION("Pamela","Roux","4/21/1946","Magician","Light","s");
-createINFORMATION("Christoph","Payne","12/3/1954","Human","Earth","s");
-createINFORMATION("Aelia","Alberhect","12/3/1954","Magician","Electricity","s");
+  createINFORMATION("Pamela","Roux","4/21/1946","Magician","Light","Upper Sixth Form Student");
+  createINFORMATION("Christoph","Payne","12/3/1954","Human","Earth","First Form Student");
+  createINFORMATION("Aelia","Alberhect","12/3/1954","Magician","Electricity","Undergradate Student");
 
+  document.getElementById('info_Aelia').style.display = "flex";
+  generateLINK('f-link-1','mainLINK');
+  generateLINK('f-link-2','eventLINK');
+  generateLINK('f-link-3','blockLINK');
+  generateLINK('link-1','mainLINK');
 
 
 }
@@ -19,7 +24,7 @@ function myF(abc){
     all[i].style.display = "none";
   }
   if (aaa.style.display == "none"){
-    aaa.style.display = "block";
+    aaa.style.display = "flex";
   }else{
     aaa.style.display = "none";
   }
@@ -61,7 +66,8 @@ function createINFORMATION(first,last, birthday, type, magic, info){
   var tableINFO ="";
   var tableID = 'tbl_'+first;
 
-tableINFO += "<tr><td>Name</td><td>"+name+"</td></tr>";
+tableINFO += "<tr><td colspan=\"2\">"+info+"</td></tr>";
+tableINFO += "<tr><td colspan=\"2\">"+name+"</td></tr>";
 tableINFO += "<tr><td>Age</td><td>"+age+"</td></tr>";
 tableINFO += "<tr><td>Birthday</td><td>"+birthday+"</td></tr>";
 tableINFO += "<tr><td>Type</td><td>"+type+"</td></tr>";
@@ -80,6 +86,12 @@ function generateCast(type){
     studentBODY[3] = new createSTUDENT('Marjorie','Wolfe','H','img/thumb_marjorie.png','');
     studentBODY[4] = new createSTUDENT('Aelia','Alberhect','M','img/thumb_aelia.png','');
     studentBODY[5] = new createSTUDENT('Rabbit','','E','img/thumb_rabbit.png','');
+    studentBODY[6] = new createSTUDENT('Rabbit','','E','img/thumb_rabbit.png','');
+    studentBODY[7] = new createSTUDENT('Rabbit','','E','img/thumb_rabbit.png','');
+    studentBODY[8] = new createSTUDENT('Rabbit','','E','img/thumb_rabbit.png','');
+    studentBODY[9] = new createSTUDENT('Rabbit','','E','img/thumb_rabbit.png','');
+    studentBODY[10] = new createSTUDENT('Rabbit','','E','img/thumb_rabbit.png','');
+    studentBODY[11] = new createSTUDENT('Rabbit','','E','img/thumb_rabbit.png','');
 
     var cast = document.getElementById('cast_thumbnail');
 
@@ -100,6 +112,38 @@ function generateCast(type){
     cast.innerHTML = listbySURNAME.sort().join('');
 }
 
+function createLINK(display,hyperlink,desc){
+  this.link = "<a href=\"" + hyperlink + "\">" + display + "</a>" + "<span>" + desc +  "</span>";
+}
+
+
+
+function generateLINK(where,links){
+  var list = "";
+  var i =0;
+  var docLINK = [];
+  if (links == "mainLINK"){
+    docLINK[0] = new createLINK('Joining',  'https://docs.google.com/document/d/1RhU5Cx8verBEfpb7okc4unbG7-e3HiY4V1ZMZtK9I8o/edit?usp=sharing#heading=h.rm60uadpnsno','How to Join');
+    docLINK[1] = new createLINK('Handbook',  'https://docs.google.com/document/d/1RhU5Cx8verBEfpb7okc4unbG7-e3HiY4V1ZMZtK9I8o/edit?usp=sharing','Main Handbook');
+    docLINK[2] = new createLINK('Chatroom',  'http://chat.deviantart.com/chat/arspactum','');
+    docLINK[3] = new createLINK('Inventory',  'https://docs.google.com/spreadsheets/d/1i-_tX5PRas9vS0IER5GGl8BUPjevELigOzsc5GnZb8I/edit?usp=sharing','Spreadsheet to Inventory');
+    docLINK[4] = new createLINK('Labyrinth', 'https://docs.google.com/spreadsheets/d/1kF-j8obN4o5cwRNt8XWM0C-GVd7hSTfpV1uHwhR93xE/edit?usp=sharing','');
+  }else if( links == "eventLINK"){
+    docLINK[0] = new createLINK('Event 001','#','Autumn Equinox');
+    docLINK[1] = new createLINK('Event 002','#','Bonfire Night');
+    docLINK[2] = new createLINK('Event 002.1','#','Souling and Guising');
+    docLINK[3] = new createLINK('Event 003','#','Beaver Moon');
+    docLINK[4] = new createLINK('Event 004','#','Square One');
+    docLINK[5] = new createLINK('Event 005','#','Valentine\'s Day Vinery');
+  }else if( links == "blockLINK"){
+    docLINK[0] = new createLINK('Block 000','#','Scuttlespore');
+    docLINK[1] = new createLINK('Block 001.1','#','Apples');
+  }
+  for(i = 0; i < docLINK.length; i++){
+    list += "<li>"+docLINK[i].link+"</li>";
+  }
+  document.getElementById(where).innerHTML += list;
+}
 
 
 
